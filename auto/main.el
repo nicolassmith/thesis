2a16a73ae8121f873ac142c9503b015a19992e85
nicolassmith <nicolas.d.smith@gmail.com>
2010-01-20 22:06:55 -0500

    not much

---
+++ auto/main.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "main"
+ (lambda ()
+    (TeX-run-style-hooks
+     "lgrind"
+     "latex2e"
+     "mitthesis12"
+     "mitthesis"
+     "12pt"
+     "vi"
+     "twoside"
+     "cover"
+     "contents"
+     "ch-intro"
+     "appa"
+     "appb"
+     "biblio")))
+
