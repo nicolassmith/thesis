601779994904089820a5664913afecf82b054a4b
nicolassmith <nicolas.d.smith@gmail.com>
2010-01-20 19:13:56 -0500

    changing the template (named chapter .tex files). starting to get the bibliography working.

---
+++ auto/_region_.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "lgrind"
+     "latex2e"
+     "mitthesis12"
+     "mitthesis"
+     "12pt"
+     "vi"
+     "twoside"
+     "cover"
+     "contents"
+     "ch-intro"
+     "appa"
+     "appb"
+     "biblio")))
+
---
+++ auto/biblio.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "biblio"
+ (lambda ()
+    (LaTeX-add-bibliographies
+     "mainb")))
+
