3429d739ecdf2492b977a8f4bf8865caacb8bd1f
nicolassmith <nicolas.d.smith@gmail.com>
2012-03-07 18:26:24 -0500

    some beginning stuff for FSR figure.

--- figs-omc/FSRfinessefig.m
+++ figs-omc/FSRfinessefig.m
@@ -1,4 +1,22 @@
-% first load data
-load FSRfinessemeas.mat
+% Does nonlinear curve fitting to 
+% determine the finesse and FSR of the OMC
+function FSRfinessefig()
 
-plot(freq,trans)
+    % first load data
+    load FSRfinessemeas.mat % important data are freq and trans
+
+    
+
+    fig = figure('visible','on');
+
+    plot(freq,trans,'b.')
+
+end
+
+% Subfunctions
+
+% this function gives theoretical transmission given some parameters
+function theoreticalTrans(f,FSR,finesse,Tscale,Toffset)
+    
+
+end
