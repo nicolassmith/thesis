c9c08f1444a0e8f8cbe15f57beec7b55d6bc8b78
nicolassmith <nicolas.d.smith@gmail.com>
2012-03-08 15:43:33 -0500

    get autogen m file from cftool.

--- figs-omc/FSRfinessefig.m
+++ figs-omc/FSRfinessefig.m
@@ -4,19 +4,51 @@ function FSRfinessefig()
 
     % first load data
     load FSRfinessemeas.mat % important data are freq and trans
-
     
+    fReal = 2*freq; % AOM double pass
 
+    % do curve fitting
+    error = @(FSR,finesse,Tscale,Toffset)ferror(trans,fReal,FSR,finesse,Tscale,Toffset);
+    
+    % start guess
+          % FSR finesse Tscale Toffset
+    x0 = [ 278 370 .03 .055 ]; 
+
+    x = fminsearch(@(y)error(y(1),y(2),y(3),y(4)),x0);
+    
+    xx = x;
+    fPlot = linspace(274,282,100);
+    transFit = theoreticalTrans(fPlot,xx(1),xx(2),xx(3),xx(4));
+    
+    disp(['finesse ' num2str(x(2))]);
+    
     fig = figure('visible','on');
 
-    plot(freq,trans,'b.')
+    plot(fReal,trans,'b.',fPlot,transFit,'r')
 
 end
 
 % Subfunctions
 
 % this function gives theoretical transmission given some parameters
-function theoreticalTrans(f,FSR,finesse,Tscale,Toffset)
+function out = theoreticalTrans(f,FSR,finesse,Tscale,Toffset)
     
+    base = zeros(size(f));
+    
+    for j = 1:length(f)
+        base(j) = 1 / ( 1 + 4/pi^2 * finesse^2 * sin(f(j)/FSR*pi)^2 );
+    end
+    
+    out = base*Tscale+Toffset;
+end
 
+% argument for fminsearch for curve fitting.
+function out = ferror(trans,f,FSR,finesse,Tscale,Toffset)
+
+    % make a subset of f for fitting
+    %fmin = 277;
+    %fmax = 279;
+
+    %ind = find(f>fmin&f<fmax);
+    out = sum((trans(ind)-theoreticalTrans(f(ind),FSR,finesse,Tscale,Toffset)).^2);
 end
