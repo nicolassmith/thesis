352fcd55ff98c20838dfa3ee6ff223af54f0edbc
Nicolas Smith <nicolas.d.smith@gmail.com>
2012-04-07 23:36:33 -0400

    getting data ready for sensitivity improvement figure.

---
+++ figs-jitter/sensimprovement.m
@@ -0,0 +1,10 @@
+function sensimprovement()
+
+%possibly helpful times: 
+% fixed post:919486633
+% hang TT0:920085981
+% 8W after thin:11/06/2009 09:08:22UTC
+% 12.6W after thin:11/06/2009 07:56:43UTC
+% keita after blades:19/09/2009 12:25:36UTC
+
+end
